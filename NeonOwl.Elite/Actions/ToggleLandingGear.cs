03d897f45a9cc6f95c5babcd61ed73c300839e79
DrPlague <drplague@tutanota.com>
2022-09-01 18:38:51 +0200

    GithubActions

---
+++ NeonOwl.Elite/Actions/ToggleLandingGear.cs
@@ -0,0 +1,20 @@
+﻿using SuchByte.MacroDeck.ActionButton;
+using SuchByte.MacroDeck.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using WindowsInput;
+
+namespace NeonOwl.Elite.Actions
+{
+    public class ToggleLandingGear : PluginAction
+    {
+        public override string Name => "Toggle Landing Gear";
+        public override string Description => "Deploy or retract landing gear.";
+        public override void Trigger(string clientId, ActionButton actionButton)
+        {
+            new InputSimulator().Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, VirtualKeyCode.VK_A).Sleep(100);
+        }
+    }
+}
---
+++ NeonOwl.Elite/Main.cs
@@ -0,0 +1,20 @@
+﻿using SuchByte.MacroDeck.Plugins;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using NeonOwl.Elite.Actions;
+using WindowsInput;
+
+namespace NeonOwl.Elite
+{
+    public class Main : MacroDeckPlugin
+    {
+        public override void Enable()
+        {
+            this.Actions = new List<PluginAction>
+            {
+                new ToggleLandingGear(),
+            };
+        }
+    }
+}
