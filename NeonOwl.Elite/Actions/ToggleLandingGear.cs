b3a1a0d8b5f7ddc500f0333a56b732db77a7c329
DrPlague <drplague@tutanota.com>
2022-09-09 16:37:59 +0200

    Loading config
    Loading config and executing shortcut

--- NeonOwl.Elite/Actions/ToggleLandingGear.cs
+++ NeonOwl.Elite/Actions/ToggleLandingGear.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using NeonOwl.Elite.Utils;
+using SuchByte.MacroDeck.Logging;
 using WindowsInput;
 
 namespace NeonOwl.Elite.Actions
@@ -12,9 +14,24 @@ namespace NeonOwl.Elite.Actions
     {
         public override string Name => "Toggle Landing Gear";
         public override string Description => "Deploy or retract landing gear.";
+
         public override void Trigger(string clientId, ActionButton actionButton)
         {
-            new InputSimulator().Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, VirtualKeyCode.VK_A).Sleep(100);
+            MacroDeckLogger.Info(PluginInstance.Main,
+                PluginInstance.EliteBindings.UserBindings.LandingGearToggle.Secondary.Key);
+            KeyConverter kc = new KeyConverter();
+            VirtualKeyCode key = kc.GetKey(PluginInstance.EliteBindings.UserBindings.LandingGearToggle.Primary.Key);
+
+            if (key == VirtualKeyCode.None)
+                key = kc.GetKey(PluginInstance.EliteBindings.UserBindings.LandingGearToggle.Secondary.Key);
+            if (key == VirtualKeyCode.None)
+            {
+                MacroDeckLogger.Error(PluginInstance.Main,
+                    "Elite Dangerous shortuct for landing gear toggle not found. Bind a key in the game settings.");
+                return;
+            }
+
+            PluginInstance.Input.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, key);
         }
     }
 }
--- NeonOwl.Elite/Main.cs
+++ NeonOwl.Elite/Main.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Reflection;
 using NeonOwl.Elite.Actions;
+using NeonOwl.Elite.Utils;
+using SuchByte.MacroDeck.Logging;
 using WindowsInput;
+using System;
+using System.Windows.Forms;
 
 namespace NeonOwl.Elite
 {
+    public static class PluginInstance
+    {
+        public static Main Main = new Main();
+        public static readonly InputSimulator Input = new InputSimulator();
+        public static readonly EliteShortcuts EliteBindings = new EliteShortcuts();
+    }
+
     public class Main : MacroDeckPlugin
     {
         public override void Enable()
         {
+            if (!PluginInstance.EliteBindings.CheckForConfig())
+            {
+                MacroDeckLogger.Error(this, "Elite Dangerous is not installed. Plugin will not work.");
+                return;
+            }
+
+            PluginInstance.EliteBindings.LoadConfig();
+
             this.Actions = new List<PluginAction>
             {
                 new ToggleLandingGear(),
